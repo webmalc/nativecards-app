8dfde1d25a2bac65d3bcc4e4d1d0ccc534f15671
webmalc <m@webmalc.pw>
2019-03-10 18:29:53 +0300

    add a blank login page

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(
+    public menuCtrl: MenuController,
+  ) { }
+
+  ionViewWillEnter() {
+    this.menuCtrl.enable(false);
+  }
+
+  ngOnInit() {
+  }
+
+}
